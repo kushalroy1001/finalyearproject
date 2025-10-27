ae43aff2b649c280a97654eb8dde07fff8c76fbe
kushalroy1001 <kushalofficial1001@gmail.com>
2025-10-27 22:53:50 +0530

    middleware change simplified

--- middleware.js
+++ middleware.js
@@ -1,22 +1,6 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: [
-    '/',
-    '/about',
-    '/plants',
-    '/plants/(.*)',
-    '/sign-in(.*)',
-    '/sign-up(.*)',
-    '/api/public(.*)',
-  ],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: [
-    '/api/webhook(.*)',
-  ],
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
